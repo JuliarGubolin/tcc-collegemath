304fe62eab83e59141c12aa9bd111042ea59536d
Wemerson Santos <wemersonmcs@outlook.com>
2022-07-14 19:15:18 -0300

    imagequestion

--- CollegeMath/CollegeMath.Domain/Entities/ImageQuestion.cs
+++ CollegeMath/CollegeMath.Domain/Entities/ImageQuestion.cs
@@ -2,6 +2,10 @@
 {
     public class ImageQuestion : EntityBase
     {
+        public int QuestionId { get; set; }
+
+        public Question Question { get; set; }
+
         public string Url { get; set; }
     }
 }
