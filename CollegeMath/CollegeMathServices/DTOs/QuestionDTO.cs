56b9a7f43f086818b2f970b33bd7b18bad9ebe3d
JuliarGubolin <julia.gubolin@unesp.br>
2022-08-19 20:40:05 -0300

    Sync

---
+++ CollegeMath/CollegeMathServices/DTOs/QuestionDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollegeMathServices.DTOs
+{
+    public class QuestionDTO
+    {
+        public int Id { get; set; }
+        
+        public DateTime CreatedDate { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+
+        public int LevelId { get; set; }
+
+        public int ContentId { get; set; }
+
+        public int QuestionTypeId { get; set; }
+    }
+}
